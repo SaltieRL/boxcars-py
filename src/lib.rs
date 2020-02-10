ffcf98a5063ae074ca1f7fa860c3aaf09e82a8e6
Justus K <justus.k@protonmail.com>
2020-02-10 22:30:25 +0100

    Add convert_to_py method

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use serde_json::Value;
 
 #[pyfunction]
 fn foo() -> usize {
@@ -10,4 +11,8 @@ fn foo() -> usize {
 fn boxcars_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(foo))?;
     Ok(())
+}
+
+fn convert_to_py(value: Value) -> PyObject {
+    match value {}
 }
